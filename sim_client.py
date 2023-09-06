92ff7d0761068e865912a6040fc26b3f8d7b775b
Neelay <neelay@baci-desktop.lbl.gov>
2023-09-05 19:02:56 -0700

    add plotting stuff to client

--- sim_client.py
+++ sim_client.py
@@ -1,9 +1,11 @@
 import socket
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 
 RECV_BUFSIZE = 65536
 DAC_SAMPLE_DT = 1/8.e9 # 8 GHz DAC sample rate
+ADC_SAMPLE_DT = 1/2.e9 # 2 GHz ADC sample rate
 
 class SimClient:
     """
@@ -40,6 +42,8 @@ class SimClient:
         if adc_stream is not None:
             assert np.all(adc_stream <= 1)
             adc_stream *= 2**15
+            if adc_delay > 0:
+                adc_stream = np.append(np.zeros(int(adc_delay//ADC_SAMPLE_DT)), adc_stream)
             dumpdict['adc_stream'] = adc_stream
 
         progdump = pickle.dumps(dumpdict)
@@ -53,6 +57,25 @@ class SimClient:
                     break
                 data += newdata                
 
-            self.dacout = pickle.loads(data)
-            if isinstance(self.dacout, Exception):
-                raise self.dacout
+            output_dict = pickle.loads(data)
+            if isinstance(output_dict, Exception):
+                raise output_dict
+            else:
+                self.dac_out = output_dict['dac_out']
+                self.dac_timesteps = np.arange(0, self.dac_out.shape[1]*DAC_SAMPLE_DT, DAC_SAMPLE_DT)
+                self.adc_timesteps = np.arange(0, self.dac_out.shape[1]*ADC_SAMPLE_DT, ADC_SAMPLE_DT)
+                self.acc = output_dict['acc']
+
+        def plot_dac_out(self, channel, start_time=0, end_time=None):
+            start_ind = start_time//DAC_SAMPLE_DT
+            if end_time is None
+                end_ind = self.dac_out.shape[1]
+            else:
+                end_ind = end_time//DAC_SAMPLE_DT
+
+            plt.plot(self.dac_timesteps[start_ind:end_ind], self.dac_out[channel, start_ind:end_ind]/(2**15-1))
+            plt.xlabel('time (s)')
+            plt.ylabel('DAC level')
+            plt.title(f'DAC channel {channel}')
+            plt.show()
+
