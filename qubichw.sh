6a0fafe27a0f09b77dc9ac139134ae33f7cf846f
Abhi Rajagopala <abhi@lbl.gov>
2023-09-13 16:48:37 -0700

    Removed ssh connection from script

--- qubichw.sh
+++ qubichw.sh
@@ -2,9 +2,5 @@
 
 # Function to connect to qubic hardware
 
-CONPASS="Qubic2023"
-DEST="user@baci-desktop.dhcp.lbl.gov"
-echo"Creating a network tunnel to the hardware"
-sshpass -p $CONPASS ssh -L 9090:localhost:9096 -N $DEST &
 echo"Launching jupyter notebook"
 jupyter notebook
